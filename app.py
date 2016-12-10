f67f8207733c08eacbe3978173aa425af846b51f
mike-a-yen <mike.a.yen@gmail.com>
2016-12-10 03:01:30 -0800

    port

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def stock_ticker():
                          plot=html)
   
 if __name__ == '__main__':
-  app.run(port=5000)
+  import os
+  port = os.environ.get('PORT',5000)
+  app.run(port=port)
