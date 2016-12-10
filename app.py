5c9bcb36b7e683e71112645cc5e61df065bd973d
mike-a-yen <mike.a.yen@gmail.com>
2016-12-10 03:06:51 -0800

    port

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def stock_ticker():
   
 if __name__ == '__main__':
   import os
-  port = os.environ.get('PORT',5000)
-  app.run(port=port)
+  port = int(os.environ.get('PORT',5000))
+  app.run(host='0.0.0.0', port=port)
